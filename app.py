b3369917087f8895e3ff6e21eccea2a4573c1010
xiaowanneng123 <lu8930343@gmail.com>
2024-07-25 18:29:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask,render_template,request
 import google.generativeai as palm
-import os
 
-api = os.getenv("MAKERSUITE_API_TOKEN")
-palm.configure(api_key=api)
+palm.configure(api_key="AIzaSyBOvs8iOUGUsCL43p6hOIqQzVzNnsd9zZI")
 model = {"model":"models/chat-bison-001"}
 
 app = Flask(__name__)
@@ -22,6 +20,10 @@ def genAI():
     q = request.form.get("q")
     r = palm.chat(**model,messages=q)
     return(render_template("genAI.html",r=r.last))
+
+@app.route("/DApp",methods=["GET","POST"])
+def DApp():
+    return(render_template("DApp.html"))
 
 if __name__ == "__main__":
     app.run()
